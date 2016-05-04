36b9da83357ec87329d6b3e877f4b3138c9a8cd3
Jaclyn Horowitz <jackiehorowitz@mac.com>
2016-05-03 23:44:57 -0400

    - Working on implementing closeness algo

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, url_for, redirect,session,flash, make_response
+from flask import Flask, request, render_template, url_for, redirect, session, flash, make_response
 from Matchmaker import Matchmaker
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
@@ -10,12 +10,26 @@ app = Flask(__name__)
 
 global matchmaker
 matchmaker = Matchmaker()
+
+
 @app.route('/')
 def index():
-	return render_template("questions.html")
-	#return redirect('https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token')
+    return render_template("questions.html")
 
-# @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
+
+# return redirect('https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token')
+
+@app.route('/results', methods=['GET', 'POST'])
+def get_results():
+    if request.method == 'POST':
+        matchmaker.current_user.preferences['days-per-week'] = request.form['form-days-per-week']
+        newlist = sorted(matchmaker.matches,
+                         key=lambda x: x.distance)
+        print(newlist)
+        return render_template('results.html', match_distance=newlist)
+
+
+        # @app.route('/login/<provider_name>/', methods=['GET', 'POST'])
 # def login(provider_name):
 #     response = make_response()
 #     result = authomatic.login(WerkzeugAdapter(request, response), provider_name)
@@ -29,28 +43,30 @@ def index():
 
 @app.route('/pynderbot', methods=['POST'])
 def pynderbot():
-	global userId, accessToken
-	userId = request.form['userId']
-	accessToken = request.form['accessToken']
-	
-	print(userId)
-	print(accessToken)
+    global userId, accessToken
+    userId = request.form['userId']
+    accessToken = request.form['accessToken']
 
-	return render_template("../../../../../Google Drive/Python Apps/LoveBot/templates/pynderbot.html")
+    print(userId)
+    print(accessToken)
+
+    return render_template("../../../../../Google Drive/Python Apps/LoveBot/templates/pynderbot.html")
 
 
 @app.route('/like')
 def like():
-	print(userId)
-	print(accessToken)
-	# session = pynder.Session(str(userId), str(accessToken))
-	# users = session.nearby_users()
-	# liked = []
-	# for user in users[:5]:
-	# 	user.like()
-	# 	liked.append(user.name)
-    #
-	# return render_template("like.html", liked=liked)
+    print(userId)
+    print(accessToken)
+
+
+# session = pynder.Session(str(userId), str(accessToken))
+# users = session.nearby_users()
+# liked = []
+# for user in users[:5]:
+# 	user.like()
+# 	liked.append(user.name)
+#
+# return render_template("like.html", liked=liked)
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 5000))
